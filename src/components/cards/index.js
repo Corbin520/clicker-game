fde68aba67c26bbed4b5879a61daebf1b1a4fb28
Corbin Brockbank <corbinbrckbnk76@gmail.com>
2019-12-11 16:53:14 -0700

    updating code

---
+++ src/components/cards/index.js
@@ -0,0 +1,21 @@
+
+// handleClick is what we are passing into the card as an on click event.
+
+// name
+
+import React from "react"
+import "./cards.css"
+
+function CreateCard (props) {
+
+    return(
+        // Pass it a function that we will use on App.js to handle the click events
+        <div className="card" onClick={() => props.handleClick(props.id)}>
+            <div className="img-container">
+                <img alt={props.name} src={props.image} />
+            </div>
+        </div>
+    )
+}
+
+export default CreateCard
